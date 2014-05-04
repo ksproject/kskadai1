50cc00b25b8b9ba7b43dac13f748a14fb0295e38
shinnosuke takeuchi <shinnosuke.takeuchi0921@gmail.com>
2014-05-04 21:33:31 +0900

    クライアント側ソースアップ

---
+++ Structural/Adapter/puttern_Adapter.php
@@ -0,0 +1,24 @@
+<?php
+// ファイルのインポート
+require_once 'baseClass.php';
+
+/**
+ * クライアント側のソース
+ * OpenFlieクラスをインスタンス化する
+ * 表示するファイルはbaseClass.phpとする
+ */
+try {
+	$show_file = new OpenFlie( './baseClass.php' );
+} catch ( Exception $err ) {
+	//dieはexit()と同じ。現在のスクリプトを終了。
+	die($err->getMessage());
+}
+
+/**
+ * プレーンテキストを表示
+ * プレーンテキストとハイライトしたファイルのそれぞれを表示する。
+ */
+$show_file->showPlain();
+echo '<hr>';//区切り
+$show_file->reOpenFile();
+?>
